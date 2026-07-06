2fa460a28ba09ab0805d937b5d2b50f987cfebe9
Gabriela Quintaes <gabrielapquintaes@gmail.com>
2026-07-06 11:33:39 -0300

    feat: expand ILeadCRMManagement interface with additional fields for document submission and installation details

--- src/types/ILead.type.ts
+++ src/types/ILead.type.ts
@@ -42,6 +42,80 @@ export interface ILeadGeolocation {
   formatted_address: string;
 }
 
+export interface ILeadCRMManagement {
+  operator_name: string;
+  transhipment_operator: boolean; // sim/não true/false
+  transhipment_operator_name: string;
+  input_at_operator: {
+    input: string; // não realizado/realizado com sucesso/registro com pendências
+    note: string;
+  };
+  debt_with_operator: {
+    debt_with_operator: string; //sem registro/não/sim
+    number_of_open_invoices: string;
+    debt_with_operator_amount: string;
+  };
+  score_serasa: string; // sem registro/ valor do score
+  score_boa_vista: string; // sem registro/ valor do score
+
+  credit_analysis: string; // sem análise/aprovado/negado/em análise
+
+  antifraude: string; //sem análise/ok/reprovado
+
+  pap_availability: string;
+
+  operator_history: {
+    history: boolean;
+    description: string;
+  };
+
+  lows_history: {
+    history: boolean;
+    amout_of_days: string;
+  };
+
+  reregistration: boolean;
+  re_registration_info: {
+    document: boolean; // cpf/cnpj
+    data: any; // vale usar um json ??  pq pode receber qualquer tipo de dado de recadastro(nome, endereço,.....) me da sua opinião aqui por favor
+    note: string;
+  };
+
+  submission_of_documents: {
+    is_submitted: boolean;
+    documents: string[];
+  };
+
+  biometrics: string; //(ok/pendente/cancelado/dispensado
+  contract: string; // pendente/enviado
+
+  installation: {
+    installation: string; // não agendado/agendado/reagendado/cliente não encontrado/ sem viabilidade no local/pendente/cancelado
+    scheduled_date: string;
+    rescheduled_date: string;
+    client_not_found_date: string;
+    notes: string;
+  };
+  order_status: string; //aberto/fechado/cancelado
+  sales_status: string; //venda realizada/venda não realizada
+  contact_history: {
+    attempt_number: string; // 1/2/3/...
+    date: string;
+    channel: string;
+    consultant_name: string;
+    status: string; // atendido/não atendido ou sem resposta
+    note: string;
+    return: string; // positivo/negativo/neutro
+    future_return: boolean; // sim/não
+    future_return_date: string;
+  };
+  consultant_name: string;
+  id_corp: string;
+  id_operator: string;
+  id_crm: string;
+  team: string;
+}
+
 export interface ILead {
   id: number;
   address: string;
@@ -106,6 +180,18 @@ export interface ILead {
   is_phone_valid: boolean;
   is_additional_phone_valid: boolean;
   whatsapp: boolean;
+
+  rg: {
+    rg: string;
+    date_of_issue: string;
+    issuing_authority: string;
+  };
+
+  link_to_registration_form: string;
+  link_to_reregistration_form: string;
+  link_to_transhipment_form: string;
+
+  crm_management: ILeadCRMManagement;
 }
 
 export interface ILeadPagination {
